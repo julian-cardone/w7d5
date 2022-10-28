8caf951fb396c01e2a0139d066db37ffd71e42e5
Julian Cardone <juliancardone1@gmail.com>
2022-10-28 17:15:45 -0400

    finished user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,35 @@
 class UsersController < ApplicationController
 
     def index
-        
+        @user = User.all
+        render :index
     end
 
-
     def new
-
+        @user = User.new
+        render :new
     end
 
     def create
+        @user = User.new(user_params)
 
-    
+        if @user.save
+            login(@user)
+            redirect_to user_url(@user.id)
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
     end
-
 
     def show
-
+        @user = User.find_by(id: params[:id])
+        render :show
     end
 
-
-
+    private
+    def user_params
+        require(:user).permit(:username, :password)
+    end
 
 end
