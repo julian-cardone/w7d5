9c4b862679d0122eb599315a964cbba6e1c3bd9c
kkj2010 <kunjukim0210@gmail.com>
2022-10-28 12:17:03 -0400

    working on controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+    #CHRLL
+    helper_method :current_user, :logged_in?
+    
+    def current_user
+        @current_user= User.find_by(session_token: session:[session_token])
+    end
+
+    def required_logged_in
+        redirect_to new_session_url unless logged_in?
+    end
+
+    def required_logged_out
+        redirect_to user_url(current_user.id) if logged_in?
+    end
+
+    def login(user)
+        session[:session_token]= user.reset_session_token!
+    end
+
+    def logout
+        session[:session_token]= nil
+        current_user.reset_session_token!
+        @current_user= nil
+
+    end
+
+    
+
 end
